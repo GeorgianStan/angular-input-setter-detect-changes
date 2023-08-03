40467938a1f4d90f45a219e55055e4559180538a
Georgian Stan <georgian.stan@accesa.eu>
2023-08-03 11:21:22 +0300

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,58 @@
+import {
+  Component,
+  Input,
+  ContentChild,
+  TemplateRef,
+  inject,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { filter, tap, fromEvent, map } from 'rxjs';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  standalone: true,
+  imports: [MatDialogModule],
+  selector: 'app-modal',
+  template: '',
+})
+export class ModalComponent {
+  #dialog = inject(MatDialog);
+
+  @Input() set isOpen(value: boolean) {
+    console.log('Modal component', value);
+    if (value && this.template) {
+      this.#dialog.open(this.template, { panelClass: '' });
+      return;
+    }
+
+    this.#dialog.closeAll();
+  }
+
+  @ContentChild(TemplateRef, { static: false }) template:
+    | TemplateRef<unknown>
+    | undefined;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, ModalComponent],
+  template: `
+    <app-modal [isOpen]="!!(source$ | async)">
+      <ng-template> <p>Modal</p></ng-template>
+    </app-modal>
+  `,
+})
+export class AppComponent {
+  source$ = fromEvent<KeyboardEvent>(document, 'keydown').pipe(
+    filter(({ code }) => code === 'Enter'),
+    map(() => true),
+    tap((val) => console.log('Emitted val', val))
+  );
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimations()],
+});
